5bd72c9478351fafe9d0169f423eea5c086f46ee
Andrew Martynyak <ibuybusinesses@gmail.com>
2024-12-17 15:44:52 -0600

    Add project files.

---
+++ ConsoleAppTest/MyStream.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest
+{
+	internal class MyStream : Stream
+	{
+		private MemoryStream _stream;
+
+		public MyStream()
+		{
+			_stream = new MemoryStream();
+		}
+
+
+		public MyStream(byte[] data)
+		{
+			_stream = new MemoryStream(data);
+		}
+		public override bool CanRead => throw new NotImplementedException();
+
+		public override bool CanSeek => throw new NotImplementedException();
+
+		public override bool CanWrite => throw new NotImplementedException();
+
+		public override long Length => throw new NotImplementedException();
+
+		public override long Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+		public override void Flush()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override int Read(byte[] buffer, int offset, int count)
+		{
+			return _stream.Read(buffer, offset, count);
+		}
+
+		public override long Seek(long offset, SeekOrigin origin)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void SetLength(long value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Write(byte[] buffer, int offset, int count)
+		{
+			_stream.Write(buffer, offset, count);
+		}
+	}
+}
---
+++ ConsoleAppTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleAppTest;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			MyStream input = new MyStream(System.Text.Encoding.UTF8.GetBytes("Hello test test test"));
+			MyStream output = new MyStream();
+			Copy(input, output);
+		}		
+
+
+		static void Copy(MyStream source, MyStream destination)
+		{
+			byte[] buffer = new byte[4096];
+			int byteread;
+			//source.CopyTo(destination);
+			byteread = source.Read(buffer, 0, buffer.Length);
+			while (byteread > 0)
+			{
+				destination.Write(buffer, 0, byteread);
+				byteread = source.Read(buffer, 0, buffer.Length);
+			}
+		}
+	}
+}
